d030c33de5f92abb99eed32d3865c04fa312ee56
Merima Cisija <mcisija1@etf.unsa.ba>
2015-06-02 21:10:32 +0200

    Windows Phone App

---
+++ KINO/KINO/MainPage.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using KINO.Resources;
+
+namespace KINO
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // Sample code to localize the ApplicationBar
+            //BuildLocalizedApplicationBar();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Pon.xaml", UriKind.Relative));
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Uto.xaml", UriKind.Relative));
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Sri.xaml", UriKind.Relative));
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Cet.xaml", UriKind.Relative));
+        }
+
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Pet.xaml", UriKind.Relative));
+        }
+
+        private void Button_Click_6(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Sub.xaml", UriKind.Relative));
+        }
+
+        private void Button_Click_7(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Ned.xaml", UriKind.Relative));
+        }
+
+        // Sample code for building a localized ApplicationBar
+        //private void BuildLocalizedApplicationBar()
+        //{
+        //    // Set the page's ApplicationBar to a new instance of ApplicationBar.
+        //    ApplicationBar = new ApplicationBar();
+
+        //    // Create a new button and set the text value to the localized string from AppResources.
+        //    ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
+        //    appBarButton.Text = AppResources.AppBarButtonText;
+        //    ApplicationBar.Buttons.Add(appBarButton);
+
+        //    // Create a new menu item with the localized string from AppResources.
+        //    ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
+        //    ApplicationBar.MenuItems.Add(appBarMenuItem);
+        //}
+    }
+}
---
+++ KINO/KINO/Pon.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Linq;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace KINO
+{
+    public partial class Pon : PhoneApplicationPage
+    {
+        public Pon()
+        {
+            InitializeComponent();
+            using (MMA_kinoContext db = new MMA_kinoContext(MMA_kinoContext.ConnectionString))
+            {
+                db.CreateIfNotExists();
+                try
+                {
+                    Table<Pon_Sala1> film = db.GetTable<Pon_Sala1>();
+                    var filmQuery = from f in film.ToList() select f;
+                    foreach (var film1 in filmQuery)
+                    {
+                        PivotItem p = new PivotItem();
+                        FilmKontorla filmKontrola = new FilmKontorla();
+                        filmKontrola.Cijena.Text = film1.Cijena + " KM";
+                        filmKontrola.Termin.Text = film1.Termin;
+                        if (film1.Slika.ToArray() != null && film1.Slika.ToArray() is Byte[])
+                        {
+                            MemoryStream stream = new MemoryStream(film1.Slika.ToArray());
+                            BitmapImage image = new BitmapImage();
+                            image.SetSource(stream);
+                            filmKontrola.Slika.Source = image;
+                        }
+                        p.Header = film1.Naziv;
+                        p.Content = filmKontrola;
+                        pivot.Items.Add(p);
+                    }
+
+                }
+                catch (Exception et)
+                {
+
+                }
+            }
+
+
+
+        }
+
+        private void Pivot_Loaded_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
